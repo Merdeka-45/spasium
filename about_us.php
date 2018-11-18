82d5698b3bfc6cc622654c7d5c72dec3a50984a8
Muhammad Wage Juli Saputra <wage.muhammad13@gmail.com>
2018-11-18 08:57:14 +0700

    update link href in menu

--- about_us.php
+++ about_us.php
@@ -1,1 +1,206 @@
-<?php include('about_us.html')?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About Us - Spasium</title>
+    
+    <!-- CSS  -->
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <!-- end of CSS  -->
+
+    <!-- Javascript -->
+    <script src="assets/js/jquery-3.3.1.min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+    <!-- end of Javascript -->
+    
+    <!-- custom javascript -->
+        <script src="assets/js/custom.js"></script>
+    <!-- end of custom javascript -->
+
+</head>
+<body>
+    <section id="landing-section-slideshow">
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="menu-spasium">
+                <a class="navbar-brand" href="/index.php">
+                    <img src="assets/images/logo-spasium.png" alt="">
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse " id="navbarNav">
+                    <ul class="navbar-nav text-center ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="/about_us.php">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Our Project</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Spasium Artist</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">3D Art</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Store</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Blog</a>
+                        </li>
+                    </ul>
+                    <div class="navbar-text search">
+                        <input type="text" placeholder="Cari" id="search-input">
+                        <img src="assets/images/search.png" alt="">
+                        <img src="assets/images/cart.png" alt="">
+                        <img src="assets/images/user.png" alt="">
+                    </div> 
+                </div>
+            </nav>
+        </div>
+    </section>
+    <section class="banner about-us-banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="text-center about-us-banner--image">
+                        <img src="assets/images/about_us/decoration.png" alt="">
+                        <img src="assets/images/about_us/about_us_banner.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="about-us-desc">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-12">
+                    <div class="row justify-content-center about-us-banner--desc">
+                        <div class="col-xl-4 left-side">
+                            <p class="bold">WE CREATE <br> A HIGH QUALITY</p>
+                            <p class="regular theme-color">ARTWORKS PRINTING <br> AND BEYOND</p>
+                        </div>
+                        <div class="col-xl-7 right-side text-justify">
+                            <p><span class="bold">SPASIUM</span> is an Indonesia-based online art gallery that is designed to help you in finding exclusive artworks for your interior, hotel, and commercial spaces. As one of the leading and the fastest growth of art platform, we offer licensed and ready-to-display artwork from selected Indonesian talents in the simplest way, vary from photography, painting, sculptures,and also wall art installation from metal, stone, brass or copper.</p>
+                            <p>With a mission to provide the simplest and most efficient solution in art inquiries, SPASIUM provides various options and materials to choose to fit your needs, including the budget-friendly solution. Please do not hesitate to contact our consultant at info@spasium.com / +628175273595, and we will do our best to assist your needs with several necessary customisation.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="milestone">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-xl-5 left-side-milestone">
+                    <p class="milestone-title bold">MILESTONE</p>
+                    <ul class="pl-3 list-milestone">
+                        <li>Represented 1.300+ collections from 100+ Indonesian visual artists (including 15 well established & professional artists), and more collections on the list.</li>
+                        <li>Various range of visual collections from photography, graphic / digital art, illustration, painting, mix media, sculptures, and wall art installation.</li>
+                        <li>Been establishing strong network and partnership among thousands of interior and architect consultant in Indonesia.</li>
+                        <li>Been featured in various interior and lifestyle online-offline media such as CLARA, Home Living, Asri, Elle Decor, Idea, Property-In, Hotelier Indonesia, Tech In Asia, Sugar & Cream, and many more.</li>
+                        <li>Delivered artworks for some hospitality projects:</li>
+                        <div class="pl-4">
+                            <li>Golden Tulip Holland Resort, Batu, Malang</li>
+                            <li>Erha Derma Clinic in Bandung, Surabaya, Medan, and more cities on the pipeline</li>
+                            <li>Neo Hotel, Pontianak</li>
+                            <li>Rattan Inn Hotel, Banjarmasin</li>
+                            <li>Sinarmas Forestry Office, BSD, Tangerang.</li>
+                        </div>
+                    </ul>
+                </div>
+                <div class="col-xl-6 right-side-milestone">
+                    <div class="image-milestone pl-4 text-center">
+                        <img src="assets/images/about_us/milestone_image.png" alt="">
+                        <img src="assets/images/about_us/decoration.png" alt="">
+                    </div>
+                    <ul class="list-milestone">
+                        <li>Atria Hotel, Serpong, Tangerang</li>
+                        <li>Graha Natura show unit, Surabaya (by Intiland Group)</li>
+                        <li>Southhills Apartment, Kuningan, Jakarta</li>
+                        <li>Sushi Hiro Restaurant, Jakarta</li>
+                        <li>Grill On Restaurant, Semarang</li>
+                        <li>and other high end residences around Indonesia</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="work-together">
+        <div class="container">
+            <div class="row justify-content-center pb-5">
+                <div class="col-xl-11 text-center">
+                    <hr class="hr-about-us">
+                    <p class="work-together-title">HAVE SOMETHING WE CAN WORK ON TOGETHER?</p>
+                    <p class="mb-5">Don't worry if you haven't. Let's just meet, talk, and start from there. <br> Because they said, everything begin with an idea, and then it becomes something else.</p>
+                    <p class="work-together-title">OR MAYBE YOU WANT TO SEE OUR WORK FIRST</p>
+                    <button class="btn btn-theme regular">LET'S VIEW PORTOFOLIO</button>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+               
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row">
+            </div>
+        </div>
+    </section>
+    <section>
+        <div class="container">
+            <div class="row"></div>
+        </div>
+    </section>
+    <section id="footer-lp">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-8 col-xl-3 col-sm-5">
+                    <p class="bold p-no-ma">Information</p>
+                    <hr class="footer--hr">
+                    <div class="row footer-menu">
+                        <div class="col-6 col-xl-6 col-sm-6 col-6">
+                            <a href="">Artworks</a>
+                            <a href="">Bussiness</a>
+                            <a href="">Delivery</a>
+                        </div>
+                        <div class="col-6 col-xl-6 col-sm-6">
+                            <a href="">Return</a>
+                            <a href="">Ordering</a>
+                            <a href="">Content Policy</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-8 col-xl-4 col-sm-5">
+                    <p class="bold p-no-ma">Contact Us</p>
+                    <hr class="footer--hr">
+                    <p>P : (+62) 882 1553 0130 | (+62) 817 527 3595</p>
+                    <p>E : info@spasium.com</p>
+                </div>
+                <div class="col-6 col-xl-3 col-sm-2 socmed-footer text-right">
+                    <div class="btn-group" role="group" aria-label="Basic example">
+                        <button type="button" class="btn btn-secondary"><img src="assets/images/socmed-fb.png" alt=""></button>
+                        <button type="button" class="btn btn-secondary"><img src="assets/images/socmed-twitter.png" alt=""></button>
+                        <button type="button" class="btn btn-secondary"><img src="assets/images/socmed-ig.png" alt=""></button>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center copyright">
+                <div class="col-md-6 text-center">
+                    Copyright &copy; 2018 by <span class="bold">Spasium</span>. All right reserved
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
